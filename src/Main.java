1190dbffa40afb9f70bc270f064963f064a8d39e
Vyach52 <Sslavih81@gmail.com>
2023-02-12 16:13:24 +0300

    Циклы часть 1

---
+++ src/Main.java
@@ -0,0 +1,69 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i = i + 1) {
+            System.out.println("Число " + i);
+        }
+        {
+            System.out.println(
+
+            );
+        }
+        {
+            for (int y = 10; y >= 1; y = y - 1) {
+                System.out.println("Число " + y);
+            }
+        }
+        {
+            System.out.println(
+
+            );
+        }
+        {
+            for (int z = 0; z <= 17; z = z + 2) {
+                System.out.println("Четное число " + z);}
+                { System.out.println(
+
+                );}
+        }
+        {
+            for (int a = 10; a >= -10; a = a-1) {
+                System.out.println(a);}
+                { System.out.println(
+
+                );}
+            }
+        {for (int b = 1904; b < 2096; b = b + 4){
+            System.out.println(b+" Год является високосным");}}
+        {
+            System.out.println(
+
+            );}
+        {for (int c = 7; c <=98; c = c + 7){
+            System.out.println(c);}}
+            {
+                System.out.println(
+
+                );}
+        {for (int d = 1; d <=512; d = d * 2){
+            System.out.println(d);}}
+        {
+            System.out.println(
+
+            );}
+        {int salary = 29000;
+        int total = 0;
+        for (int i = 1; i <= 12; i++) {
+            total = total + total/100;
+            total = total + salary;
+        System.out.println("Месяц "+ i + " Сумма накоплений " + total);}
+        { System.out.println(
+
+        );}
+            { int i=2;
+                int b;
+            for (int example = 1; example <= 10; example ++) {
+                b = example*i;
+                System.out.println("2*" + example + "=" + b);}}
+        }}}
+
+
